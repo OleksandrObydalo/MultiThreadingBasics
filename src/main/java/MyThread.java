233269f5afa86efb722242eb393a925e89364486
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-25 09:41:00 +0300

    Call sleep() in MyThread class

--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -2,11 +2,18 @@ public class MyThread extends Thread {
     @Override
     public void run() {
         System.out.println("Потік " + getName() + " працює!");
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Круто!");
     }
 
     public static void main(String[] args) {
         MyThread thread = new MyThread();
         thread.start();  // запускаємо потік
+        System.out.println("Потік працює паралельно");
     }
 }
 
