0417a108ff2aeb79f63e6d66c758af67a65da9a3
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-25 08:59:30 +0300

    Create Thread using inheritance

---
+++ src/main/java/MyThread.java
@@ -0,0 +1,12 @@
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Потік " + getName() + " працює!");
+    }
+
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();  // запускаємо потік
+    }
+}
+
